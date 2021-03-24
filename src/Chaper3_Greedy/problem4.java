7ef93a75b27a81a92f444facf7b43c8d016e619c
JGeun <pjk8926@gmail.com>
2021-03-24 16:43:32 +0900

    1이 될 때까지

---
+++ src/Chaper3_Greedy/problem4.java
@@ -0,0 +1,25 @@
+package Chaper3_Greedy;
+
+import java.util.Scanner;
+
+public class problem4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int result = 0;
+
+        while(true){
+            int target = (N/K)*K;
+            result += (N-target);
+            N = target;
+            if(N<K)
+                break;
+            result += 1;
+            N /= K;
+        }
+
+        result += (N-1);
+        System.out.println(result);
+    }
+}
