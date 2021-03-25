32708a05cae2bb7223f15b4fa5a7502a928c87e3
JGeun <pjk8926@gmail.com>
2021-03-25 14:47:16 +0900

    왕실의 나이트

---
+++ src/Chapter4_Realization/problem3.java
@@ -0,0 +1,26 @@
+package Chapter4_Realization;
+
+import java.util.Scanner;
+
+public class problem3 {
+    private static int[] dx = {-2, -1, 1, 2, 2, 1, -1, -2};
+    private static int[] dy = {-1, -2, -2, -1, 1, 2, 2, 1};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String position = sc.nextLine();
+        int row = position.charAt(1) - '0';
+        int column = position.charAt(0) - 'a' + 1;
+
+        int result = 0;
+        for(int i=0; i<8; i++){
+            int nextRow = row + dx[i];
+            int nextColumn = column + dy[i];
+
+            if(nextRow >= 1 && nextRow <= 8 && nextColumn >= 1 && nextColumn<=8)
+                result+=1;
+        }
+
+        System.out.println(result);
+    }
+}
